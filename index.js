8d44e3ca22b69bb8ffd1085df6b0c21f92b3001f
menyagah <menyagah27@gmail.com>
2021-12-04 10:37:45 +0300

    Add bulma css tags

--- index.js
+++ index.js
@@ -79,10 +79,10 @@ const onMovieSelect = async movie => {
 
 const movieTemplate = (movieDetail) => {
     return `
-    <article class="media">
-        <figure class="media-left">
-            <p class="image">
-                <img src="${movieDetail.Poster}"/>
+    <article class="media pb-3">
+        <figure class="media-left h-6 w-6">
+            <p class="image ">
+                <img src="${movieDetail.Poster}" />
             </p>
         </figure>
         <div class="media-content">
@@ -90,28 +90,28 @@ const movieTemplate = (movieDetail) => {
                 <h1>${movieDetail.Title}</h1>
                 <h4>${movieDetail.Genre}</h4>
                 <p>${movieDetail.Plot}</p>
-            <div>
+            </div>
         </div>
     </article>
     <article class ="notification is-primary">
         <p class="title">${movieDetail.Awards}</p>
-        <p class="title">Awards</p>
+        <p class="subtitle">Awards</p>
     </article>
     <article class ="notification is-primary">
         <p class="title">${movieDetail.BoxOffice}</p>
-        <p class="title">Box Office</p>
+        <p class="subtitle">Box Office</p>
     </article>
     <article class ="notification is-primary">
         <p class="title">${movieDetail.Metascore}</p>
-        <p class="title">Metascore</p>
+        <p class="subtitle">Metascore</p>
     </article>
     <article class ="notification is-primary">
         <p class="title">${movieDetail.imdbRating}</p>
-        <p class="title">IMDB Rating</p>
+        <p class="subtitle">IMDB Rating</p>
     </article>
     <article class ="notification is-primary">
         <p class="title">${movieDetail.imdbVotes}</p>
-        <p class="title">IMDB Votes</p>
+        <p class="subtitle">IMDB Votes</p>
     </article>
     `;
 }
